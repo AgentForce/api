b985be13065b32ea38f991244d70f2717f676030
Tu Nguyen <tunguyenq@gmail.com>
2018-03-13 11:08:50 +0700

    edit mock api

--- src/controller/activities/activity-controller.ts
+++ src/controller/activities/activity-controller.ts
@@ -93,24 +93,27 @@ export default class ActivitiesController {
     public async activitiesDay(request: Hapi.Request, reply: Hapi.ReplyNoContinue) {
         try {
             let res = {
-                statusCode: 1,
-                data: [{
-                    Id: 1,
-                    ProcessStep: 1,
-                    Type: 1,
-                    Phone: '01694248887',
-                    Name: 'TuNguyen',
-                    StartDate: '2018-01-26',
-                    FullDate: true,
-                }, {
-                    Id: 1,
-                    ProcessStep: 1,
-                    Type: 1,
-                    Phone: '01694248888',
-                    Name: 'John',
-                    StartDate: '2018-01-26',
-                    FullDate: true,
-                }]
+                "statusCode": 1,
+                "data": [
+                    {
+                        "Status": false,
+                        "StartDate": "2018-11-11T00:00:00.000Z",
+                        "ProcessStep": 0,
+                        "manulife_lead": {
+                            "Name": "1"
+                        }
+                    },
+                    {
+                        "Status": false,
+                        "StartDate": "2018-11-11T00:00:00.000Z",
+                        "ProcessStep": 0,
+                        "manulife_lead": {
+                            "Name": "1"
+                        }
+                    }
+                ],
+                "msgCode": "success",
+                "msg": "success"
             };
             reply(res);
         } catch (ex) {
